6ed6725fd3bb5a2f3a9c2af571f115fd7a7bc6ec
Marek Wasielewski <marek.wasielewski@telemetria.eu>
2016-08-03 23:32:35 +0200

    Test

--- test9510 API/test9510 API/Service1.svc.cs
+++ test9510 API/test9510 API/Service1.svc.cs
@@ -29,7 +29,7 @@ namespace test9510API
         public void DeleteAMachine(string name)
         {
             var context = WebOperationContext.Current;
-            context.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.Accepted;
+            context.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.NoContent;
         }
 
         public test9510API.Models.Machine[] GetAllMachines()
