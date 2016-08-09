cbacebe11c82c738e1e947eda9c62e0ede028ad9
Marek Wasielewski <marek.wasielewski@telemetria.eu>
2016-08-09 15:20:32 +0200

    AA

--- test9510 API/test9510APITests/Service1Tests.cs
+++ test9510 API/test9510APITests/Service1Tests.cs
@@ -11,11 +11,21 @@ namespace test9510API.Tests
     public class Service1Tests
     {
         [TestMethod()]
-        public void DoWorkTest()
+        public void DeleteAMachine()
         {
             var service = new Service1();
             service.DeleteAMachine("Willi");
             Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetAMachine()
+        {
+            var service = new Service1();
+            var machine = service.GetAMachine("willy");
+            //if(machine.type == "bulldozer" && machine.name == "willy" && machine._id == "5229c6e8e4b0bd7dbb07e29c")
+
+            Assert.IsTrue(false);
         }
     }
 }
