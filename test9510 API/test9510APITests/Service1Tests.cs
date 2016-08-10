a22607382cfa2ae9a5888451d36232f1d88da94d
Marek Wasielewski <marek.wasielewski@telemetria.eu>
2016-08-10 14:12:02 +0200

    aa

--- test9510 API/test9510APITests/Service1Tests.cs
+++ test9510 API/test9510APITests/Service1Tests.cs
@@ -38,7 +38,7 @@ namespace test9510API.Tests
                 && machines.First().type == "bulldozer"
                 && machines.First().name == "willy"
                 && machines.First()._id == "52341870ed55224b15ff07ef")
-                Assert.IsTrue(true);
+                Assert.IsTrue(false);
             else
                 Assert.IsTrue(false);
         }
