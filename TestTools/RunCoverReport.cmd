8a56101886f76d50b588577a3c2905c92822e45d
Marek Wasielewski <marek.wasielewski@telemetria.eu>
2016-08-12 10:29:58 +0200

    Test tols

--- TestTools/RunCoverReport.cmd
+++ TestTools/RunCoverReport.cmd
@@ -1,2 +1,2 @@
 @ECHO ON
-"%~dp0CoverReportGenerator.cmd"  "%~dp0..\test9510 API\test9510APITests\bin\Debug\test9510APITests.dll" 1
+"%~dp0CoverReportGenerator.cmd"  "%~dp0..\test9510 API\test9510APITests\bin\Release\test9510APITests.dll" 1
